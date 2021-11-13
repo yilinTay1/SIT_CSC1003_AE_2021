591c8470fc18b98e3482a8312ea6e90d44cbdd4c
Yi Lin ♡ <69491858+yilinTay1@users.noreply.github.com>
2021-11-13 15:24:22 +0800

    Update probError.c

--- probError.c
+++ probError.c
@@ -4,7 +4,7 @@
 /*************************************************************/
 //Count the number of error for Testing dataset
 errorProbability_=0;
-for (i=0;i<20;i++)
+for (i=0;i< 20;i++)
 {
     errorProbability+=errortest[i][0];
 }
@@ -18,4 +18,17 @@ for (i = 0; i < 80; i++)
     errorProbability += errortrain[i][0];
 }
 errorProbability = errorProbability/80;
+//Confusion matrix
 printf("\nTraining error =%lf", errorper);
+
+printf("\n________________________________________\n") ;
+printf(" Confusion Matrix - Testing dataset");
+printf("\n_________________________________________\n") ;
+printf("                     Actual Positive            Actual Negative ");
+printf(" Predicted Positive      %d(True Positive)                           %d(False Negative)      ");
+printf(" Predicted Negative      %d(False Positive)                           %d(True Negative)        ");
+
+int true_Positive //No of times predict correctly that patient is not normal
+int  //No of times predict wrongly that patient is not normal butpatient is actually normal
+int false_Negative //No of times predict wrongly that patient is normal but patient is actually not normal
+int true_Negative //No of times predict correctly that patient is normal
